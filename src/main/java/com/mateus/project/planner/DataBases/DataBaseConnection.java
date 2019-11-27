9244406dafb6cc1b803824622a449856f746a62f
mateusdalves <alves.mateus@protonmail.com>
2019-11-27 17:21:40 -0300

    Update DataBaseConnection.java

--- src/main/java/com/mateus/project/planner/DataBases/DataBaseConnection.java
+++ src/main/java/com/mateus/project/planner/DataBases/DataBaseConnection.java
@@ -1,4 +1,22 @@
 package com.mateus.project.planner.DataBases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+    private final String username;
+    private final String pass;
+
+    public DataBaseConnection(){
+        this.username = "planner";
+        this.pass = "planner@2135";
+    }
+
+    public Connection connect() throws SQLException {
+            String dbURL = "jdbc:mysql://localhost:3306/planner-db";
+            return DriverManager.getConnection(dbURL, this.username, this.pass);
+    }
+
+
 }
